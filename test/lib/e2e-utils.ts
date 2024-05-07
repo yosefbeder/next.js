19b06dd0e234a0b408f37cbfb12919076252ce4c
Tobias Koppers <tobias.koppers@googlemail.com>
2024-05-07 17:38:57 +0200

    fix isTurbopack check in test cases (#65446)
    ### What?
    
    Fix the `isTurbopack` flag in production test cases

--- test/lib/e2e-utils.ts
+++ test/lib/e2e-utils.ts
@@ -282,9 +282,7 @@ export function nextTestSetup(
     },
     get isTurbopack(): boolean {
       return Boolean(
-        isNextDev &&
-          !process.env.TEST_WASM &&
-          (options.turbo ?? shouldRunTurboDevTest())
+        !process.env.TEST_WASM && (options.turbo ?? shouldRunTurboDevTest())
       )
     },
 
