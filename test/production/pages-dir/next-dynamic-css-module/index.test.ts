6828add02f45216c047d5616e188c0252450a211
devjiwonchoi <devjiwonchoi@gmail.com>
2024-08-01 22:57:45 +0900

    test: rename test

--- test/production/pages-dir/next-dynamic-css-module/index.test.ts
+++ test/production/pages-dir/next-dynamic-css-module/index.test.ts
@@ -1,19 +1,25 @@
 import { nextTestSetup } from 'e2e-utils'
-import { check } from 'next-test-utils'
 
 describe('next-dynamic-33286', () => {
   const { next } = nextTestSetup({
     files: __dirname,
   })
 
-  it('should work using browser', async () => {
+  it('should be able to load the same css module with both next dynamic and import', async () => {
     const browser = await next.browser('/')
     await browser.elementByCss('a').click()
-    await check(async () => {
-      return await browser.eval(
+
+    expect(
+      await browser.eval(
+        `window.getComputedStyle(document.querySelector('button')).backgroundColor`
+      )
+    ).not.toBe('rgb(239, 239, 239)')
+
+    expect(
+      await browser.eval(
         // expect the button's background to be red
         `window.getComputedStyle(document.querySelector('button')).backgroundColor`
       )
-    }, 'rgb(255, 0, 0)')
+    ).toBe('rgb(255, 0, 0)')
   })
 })
