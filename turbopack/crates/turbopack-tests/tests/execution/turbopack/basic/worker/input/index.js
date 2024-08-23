768d5e4bdc29afd9a637e3d0e5f83f6c65521177
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2024-08-23 20:54:49 +0200

    WIP test

--- turbopack/crates/turbopack-tests/tests/execution/turbopack/basic/worker/input/index.js
+++ turbopack/crates/turbopack-tests/tests/execution/turbopack/basic/worker/input/index.js
@@ -1,15 +1,16 @@
 // patch Workers for our Node.js test environment
-globalThis.Worker = (Date.now() > 0 ? require : 'unused')(
-  'node:worker_threads'
-).Worker
+// globalThis.Worker = (Date.now() > 0 ? require : 'unused')(
+//   'node:worker_threads'
+// ).Worker
 
 it('supports workers', async () => {
-  let worker = new Worker(new URL('./worker.ts', import.meta.url))
-  let message = await new Promise((resolve) => {
-    worker.addEventListener('message', (event) => {
-      resolve(event.data)
-    })
-  })
+   new Worker(new URL('./worker.ts', import.meta.url))
+  // let worker = new Worker(new URL('./worker.ts', import.meta.url))
+  // let message = await new Promise((resolve) => {
+  //   worker.addEventListener('message', (event) => {
+  //     resolve(event.data)
+  //   })
+  // })
 
-  expect(message).toBe('getMessage worker-dep')
+  // expect(message).toBe('getMessage worker-dep')
 })
